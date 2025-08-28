d3b6d4553e366ade2c0d7f6477ef6c778bf5f231
moonthang <mspulveda@ucompensar.edu.co>
2025-08-28 18:12:50 -0500

    Cambio de los activity  de constraintlayout a ScrollView

--- app/src/main/java/com/example/instagramchino/Home.kt
+++ app/src/main/java/com/example/instagramchino/Home.kt
@@ -1,6 +1,8 @@
 package com.example.instagramchino
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ class Home : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnComenzar = findViewById<Button>(R.id.btnComenzar)
+        btnComenzar.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/instagramchino/Splash.kt
+++ app/src/main/java/com/example/instagramchino/Splash.kt
@@ -3,13 +3,14 @@ package com.example.instagramchino
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Splash : AppCompatActivity() {
+    private val splashTimeOut: Long = 3000 // 3 segundos
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,5 +20,11 @@ class Splash : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        Handler().postDelayed({
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
+            finish()
+        }, splashTimeOut)
     }
 }
